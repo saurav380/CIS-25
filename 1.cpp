4a34ccc2ec2f8d9c54281e5dfd6491b926a23301
saurav380 <sauravpoudel3330@gmail.com>
2024-09-04 09:40:12 -0700

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+void printValues() {
+    int num1 = 2;
+    int num2 = 1;
+    cout << "num1 = " << num1 << " and num2 = " << num2 << endl;
+}
+
+int main() {
+    printValues();
+    return 0;
+}
